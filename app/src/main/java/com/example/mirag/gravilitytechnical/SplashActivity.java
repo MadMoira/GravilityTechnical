5a2adf1d7d98dd95e36e232a20265210f0662df9
Camilo Rodríguez <holiwords@gmail.com>
2016-10-05 07:00:07 -0500

    Basic skeleton and splash view

--- app/src/main/java/com/example/mirag/gravilitytechnical/SplashActivity.java
+++ app/src/main/java/com/example/mirag/gravilitytechnical/SplashActivity.java
@@ -1,13 +1,28 @@
 package com.example.mirag.gravilitytechnical;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Duration of the Splash Screen
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, CategoriesListActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
